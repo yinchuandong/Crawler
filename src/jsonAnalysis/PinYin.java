ad284f93cca82ffab83454e9e44cefddb6fdaee6
yinchuandong <yincd520@sina.com>
2015-04-15 11:55:26 +0800

    format PinYin.java

--- src/jsonAnalysis/PinYin.java
+++ src/jsonAnalysis/PinYin.java
@@ -5,53 +5,54 @@ import net.sourceforge.pinyin4j.format.HanyuPinyinCaseType;
 import net.sourceforge.pinyin4j.format.HanyuPinyinOutputFormat;
 import net.sourceforge.pinyin4j.format.HanyuPinyinToneType;
 
-
 public class PinYin {
-	
+
 	public String pinyinName = "";
-	public String HanyuToPinyin(String name){
-		 	
-	        char[] nameChar = name.toCharArray();
-	        HanyuPinyinOutputFormat defaultFormat = 
-	                                           new HanyuPinyinOutputFormat();
-	        defaultFormat.setCaseType(HanyuPinyinCaseType.LOWERCASE);
-	        defaultFormat.setToneType(HanyuPinyinToneType.WITHOUT_TONE);
-	        for (int i = 0; i < nameChar.length; i++) {
-	            if (nameChar[i] > 128) {
-	                try {
-	                    pinyinName += PinyinHelper.toHanyuPinyinStringArray
-	                                           (nameChar[i], defaultFormat)[0];
-	                } catch (Exception e) {
-	                    e.printStackTrace();
-	                }
-	            } 
-	        }
-	        return pinyinName;
-	    }
-	    public static String converterToSpell(String chines){    	 
-	        String pinyinName = "";
-	        char[] nameChar = chines.toCharArray();
-	        HanyuPinyinOutputFormat defaultFormat = new HanyuPinyinOutputFormat();
-	        defaultFormat.setCaseType(HanyuPinyinCaseType.LOWERCASE);
-	        defaultFormat.setToneType(HanyuPinyinToneType.WITHOUT_TONE);
-	        for (int i = 0; i < nameChar.length; i++) {
-	            if (nameChar[i] > 128) {
-	                try {
-	                    pinyinName += PinyinHelper.toHanyuPinyinStringArray(nameChar[i], defaultFormat)[0];
-	                } catch (Exception e) {
-	                    e.printStackTrace();
-	                }
-	            }else{
-	            	pinyinName += nameChar[i];
-	            }
-	        }
-	        return pinyinName;
-	    }
-	    
+
+	public String HanyuToPinyin(String name) {
+
+		char[] nameChar = name.toCharArray();
+		HanyuPinyinOutputFormat defaultFormat = new HanyuPinyinOutputFormat();
+		defaultFormat.setCaseType(HanyuPinyinCaseType.LOWERCASE);
+		defaultFormat.setToneType(HanyuPinyinToneType.WITHOUT_TONE);
+		for (int i = 0; i < nameChar.length; i++) {
+			if (nameChar[i] > 128) {
+				try {
+					pinyinName += PinyinHelper.toHanyuPinyinStringArray(
+							nameChar[i], defaultFormat)[0];
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return pinyinName;
+	}
+
+	public static String converterToSpell(String chines) {
+		String pinyinName = "";
+		char[] nameChar = chines.toCharArray();
+		HanyuPinyinOutputFormat defaultFormat = new HanyuPinyinOutputFormat();
+		defaultFormat.setCaseType(HanyuPinyinCaseType.LOWERCASE);
+		defaultFormat.setToneType(HanyuPinyinToneType.WITHOUT_TONE);
+		for (int i = 0; i < nameChar.length; i++) {
+			if (nameChar[i] > 128) {
+				try {
+					pinyinName += PinyinHelper.toHanyuPinyinStringArray(
+							nameChar[i], defaultFormat)[0];
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			} else {
+				pinyinName += nameChar[i];
+			}
+		}
+		return pinyinName;
+	}
+
 	public static void main(String[] args) {
-		String str="Ô¬ÔÆ123lalala";
-		System.out.println(new PinYin().converterToSpell(str) );
+		String str = "Ô¬ÔÆ123lalala";
+		System.out.println(new PinYin().converterToSpell(str));
 
 	}
-	
+
 }
